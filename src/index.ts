128e695caccdeaf770d434c69d3faf756b65ca7a
junior-boop <juniorseppo3@gmail.com>
2024-03-05 04:10:40 +0100

    bible-ref-complete

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import filtre from './livre'
 
 const app = new Hono()
 
@@ -7,6 +8,15 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+app.get('/bible-ref', async ({json, text, req}) => {
+	const {book, chap, v1, v2} = req.queries()
+	
+	const result = filtre({livre : book[0], chap : chap[0], vers1 : v1[0], vers2 : v2[0] === 'undefined' ? undefined : v2[0]})
+
+	console.log({book, chap, v1, v2, result})
+
+	return json(result)
+})
 const port = 3000
 console.log(`Server is running on port ${port}`)
 
